5253e2ad0a1a44efce93efd15ab5c4d3ffc7bc98
JohnDennehy <johndennehy101@gmail.com>
2021-09-29 22:53:46 +0100

    Added base menu functionality

--- app/src/main/java/ie/wit/runappv1/activities/RaceActivity.kt
+++ app/src/main/java/ie/wit/runappv1/activities/RaceActivity.kt
@@ -1,6 +1,9 @@
 package ie.wit.runappv1.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import ie.wit.runappv1.R
 import ie.wit.runappv1.main.MainApp
 import ie.wit.runappv1.databinding.ActivityRaceBinding
 
@@ -13,8 +16,20 @@ class RaceActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRaceBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.toolbarAdd.title = title
+        setSupportActionBar(binding.toolbarAdd)
 
         app = application as MainApp
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_race, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_cancel -> { finish() }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/ie/wit/runappv1/activities/RaceListActivity.kt
+++ app/src/main/java/ie/wit/runappv1/activities/RaceListActivity.kt
@@ -1,6 +1,7 @@
 package ie.wit.runappv1.adapters
 
 import RaceAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,6 +12,10 @@ import ie.wit.runappv1.databinding.ActivityRaceListBinding
 import ie.wit.runappv1.databinding.CardRaceBinding
 import ie.wit.runappv1.main.MainApp
 import ie.wit.runappv1.models.RaceModel
+import android.view.Menu
+import android.view.MenuItem
+import ie.wit.runappv1.R
+import ie.wit.runappv1.activities.RaceActivity
 
 
 class RecipeListActivity : AppCompatActivity() {
@@ -22,12 +27,28 @@ class RecipeListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRaceListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.toolbar.title = title
+        setSupportActionBar(binding.toolbar)
 
         app = application as MainApp
 
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = RaceAdapter()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_add -> {
+                val launcherIntent = Intent(this, RaceActivity::class.java)
+                startActivityForResult(launcherIntent,0)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
 
