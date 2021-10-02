5ec5ecceb0cc01f7077550ea483208898c6438e1
JohnDennehy <johndennehy101@gmail.com>
2021-10-02 21:47:06 +0100

    Added basic splash screen (made this main activity so first thing that is shown to user). Delay in place which then transitions to raceListActivity.

---
+++ app/src/main/java/ie/wit/runappv1/activities/SplashActivity.kt
@@ -0,0 +1,21 @@
+package ie.wit.runappv1.activities
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import ie.wit.runappv1.R
+
+class SplashActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, RaceListActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
