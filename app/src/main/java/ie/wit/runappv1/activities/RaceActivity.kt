2fb7259b433234e029ff4f23132c56c05f4b846d
JohnDennehy <johndennehy101@gmail.com>
2021-10-02 13:35:50 +0100

    Reworked date picker to user material UI datepicker. Added functionality to prevent selection of date before current date.

--- app/src/main/java/ie/wit/runappv1/activities/RaceActivity.kt
+++ app/src/main/java/ie/wit/runappv1/activities/RaceActivity.kt
@@ -1,33 +1,25 @@
 package ie.wit.runappv1.activities
-import android.app.DatePickerDialog
-import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.DatePicker
-import android.widget.TimePicker
+import com.google.android.material.datepicker.CalendarConstraints
+
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.snackbar.Snackbar
 import ie.wit.runappv1.R
 import ie.wit.runappv1.main.MainApp
 import ie.wit.runappv1.databinding.ActivityRaceBinding
 import ie.wit.runappv1.models.RaceModel
-import java.time.Month
+import java.time.LocalDateTime
 import java.util.*
 
-class RaceActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+class RaceActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRaceBinding
     var race = RaceModel()
-    var day = 0
-    var month = 0
-    var year = 0
-    var hour = 0
-    var minute = 0
-    var savedDay = 0
-    var savedMonth = 0
-    var savedYear = 0
-    var savedHour = 0
-    var savedMinute = 0
     lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,7 +40,31 @@ class RaceActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener, Ti
             binding.btnAdd.setText("Edit Race")
         }
 
-        pickDate()
+        val c = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+
+        val builder :
+                MaterialDatePicker.Builder<Long> = MaterialDatePicker.Builder.datePicker()
+        builder.setTitleText("Select a Date")
+        builder.setCalendarConstraints(
+            limitRange().build()
+        )
+
+
+        val picker : MaterialDatePicker<Long> = builder.build()
+
+        binding.raceDatePicker.setOnClickListener {
+
+            picker.show(supportFragmentManager, picker.toString())
+        }
+
+        picker.addOnPositiveButtonClickListener {
+            val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+            calendar.time = Date(it)
+
+            binding.raceDatePicker.setText("${calendar.get(Calendar.DAY_OF_MONTH)}/" +
+                    "${calendar.get(Calendar.MONTH) + 1}/${calendar.get(Calendar.YEAR)}")
+        }
+
 
         binding.btnAdd.setOnClickListener() {
             race.title = binding.raceTitle.text.toString()
@@ -82,39 +98,71 @@ class RaceActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener, Ti
         return super.onOptionsItemSelected(item)
     }
 
-    private fun getDateTimeCalendar() {
-        val cal: Calendar = Calendar.getInstance()
-        day = cal.get(Calendar.DAY_OF_MONTH)
-        month = cal.get(Calendar.MONTH)
-        year = cal.get(Calendar.YEAR)
-        hour = cal.get(Calendar.HOUR)
-        minute = cal.get(Calendar.MINUTE)
+    private fun limitRange(): CalendarConstraints.Builder {
 
+        val constraintsBuilderRange = CalendarConstraints.Builder()
+
+        val calendarStart: Calendar = GregorianCalendar.getInstance()
+        val calendarEnd: Calendar = GregorianCalendar.getInstance()
+
+
+        val currentDateTime = LocalDateTime.now()
+
+
+        val year = currentDateTime.toString().substring(0,4).toInt()
+        val day = currentDateTime.toString().substring(8,10).toInt()
+        System.out.println(day)
+        val month = currentDateTime.toString().substring(5,7).toInt()
+        System.out.println(month)
+
+        calendarStart.set(year, month -1, day)
+        calendarEnd.set(year + 1, 12, 31)
+
+        val minDate = calendarStart.timeInMillis
+        val maxDate = calendarEnd.timeInMillis
+
+        constraintsBuilderRange.setStart(minDate)
+        constraintsBuilderRange.setEnd(maxDate)
+
+        constraintsBuilderRange.setValidator(RangeValidator(minDate, maxDate))
+
+        return constraintsBuilderRange
     }
 
-    private fun pickDate() {
-        binding.raceDatePickerButton.setOnClickListener {
-            getDateTimeCalendar()
-            DatePickerDialog(this,this,year,month,day).show()
+    class RangeValidator(private val minDate:Long, private val maxDate:Long) : CalendarConstraints.DateValidator{
+
+
+        constructor(parcel: Parcel) : this(
+            parcel.readLong(),
+            parcel.readLong()
+        )
+
+        override fun writeToParcel(dest: Parcel?, flags: Int) {
+            TODO("not implemented")
         }
+
+        override fun describeContents(): Int {
+            TODO("not implemented")
+        }
+
+        override fun isValid(date: Long): Boolean {
+            return !(minDate > date || maxDate < date)
+
+        }
+
+        companion object CREATOR : Parcelable.Creator<RangeValidator> {
+            override fun createFromParcel(parcel: Parcel): RangeValidator {
+                return RangeValidator(parcel)
+            }
+
+            override fun newArray(size: Int): Array<RangeValidator?> {
+                return arrayOfNulls(size)
+            }
+        }
+
     }
 
-    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        savedDay = dayOfMonth
-        savedMonth = month + 1
-        savedYear = year
 
-        getDateTimeCalendar()
-        TimePickerDialog(this,this,hour,minute,true).show()
-
-    }
-
-    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
-        savedHour = hourOfDay
-        savedMinute = minute
-
-        binding.test.setText("$savedDay + $savedMonth + $savedYear + $savedHour + $savedMinute")
-    }
 
 
 }
