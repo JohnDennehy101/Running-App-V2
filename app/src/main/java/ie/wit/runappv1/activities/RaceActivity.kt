406517e64f6a273ffe385952cb6a23795c38d51f
JohnDennehy <johndennehy101@gmail.com>
2021-10-10 16:24:39 +0100

    Added support for saving image taken with camera in app. Added additional functions to map Bitmap of taken image to uri that can be used within app for obtaining images.

--- app/src/main/java/ie/wit/runappv1/activities/RaceActivity.kt
+++ app/src/main/java/ie/wit/runappv1/activities/RaceActivity.kt
@@ -4,9 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
 import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
@@ -28,6 +25,9 @@ import java.util.*
 import android.R.attr.data
 import android.net.Uri
 import android.R.attr.data
+import android.content.ContentValues
+import android.content.Context
+import android.os.*
 import android.view.View
 import android.widget.LinearLayout
 import timber.log.Timber.i
@@ -36,6 +36,10 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.core.net.toUri
 import com.squareup.picasso.Picasso
 import ie.wit.runappv1.helpers.showImagePicker
+import java.io.File
+import java.io.File.separator
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 
 class RaceActivity : AppCompatActivity() {
@@ -88,6 +92,12 @@ class RaceActivity : AppCompatActivity() {
                 Picasso.get()
                     .load(race.image.toUri())
                     .into(binding.imageView)
+
+                val params = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    800
+                )
+                binding.imageView.layoutParams = params
                 binding.imageView.visibility = View.VISIBLE
             }
         }
@@ -149,6 +159,9 @@ class RaceActivity : AppCompatActivity() {
                 val data: Intent? = result.data
                 val takenImage = data?.extras?.get("data") as Bitmap
                 binding.imageView.setImageBitmap(takenImage)
+                val imageUri = takenImage.saveImage(applicationContext)
+                race.image = imageUri.toString()
+
                 val params = LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     800
@@ -177,6 +190,57 @@ class RaceActivity : AppCompatActivity() {
 
 
     }
+
+    fun Bitmap.saveImage(context: Context): Uri? {
+        if (android.os.Build.VERSION.SDK_INT >= 29) {
+            val values = ContentValues()
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+            values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000)
+            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/running_app_pictures")
+            values.put(MediaStore.Images.Media.IS_PENDING, true)
+            values.put(MediaStore.Images.Media.DISPLAY_NAME, "img_${SystemClock.uptimeMillis()}")
+
+            val uri: Uri? =
+                context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            if (uri != null) {
+                saveImageToStream(this, context.contentResolver.openOutputStream(uri))
+                values.put(MediaStore.Images.Media.IS_PENDING, false)
+                context.contentResolver.update(uri, values, null, null)
+                return uri
+            }
+        } else {
+            val directory =
+                File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString() + separator + "test_pictures")
+            if (!directory.exists()) {
+                directory.mkdirs()
+            }
+            val fileName =  "img_${SystemClock.uptimeMillis()}"+ ".jpeg"
+            val file = File(directory, fileName)
+            saveImageToStream(this, FileOutputStream(file))
+            if (file.absolutePath != null) {
+                val values = ContentValues()
+                values.put(MediaStore.Images.Media.DATA, file.absolutePath)
+                // .DATA is deprecated in API 29
+                context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+                return Uri.fromFile(file)
+            }
+        }
+        return null
+    }
+
+
+    fun saveImageToStream(bitmap: Bitmap, outputStream: OutputStream?) {
+        if (outputStream != null) {
+            try {
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                outputStream.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
 
 
 
