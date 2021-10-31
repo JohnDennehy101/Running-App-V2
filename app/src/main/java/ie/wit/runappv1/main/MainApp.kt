edf99e2a55cd05ecc4cb8be992f97f767579e2c8
JohnDennehy <johndennehy101@gmail.com>
2021-10-31 18:49:28 +0000

    Removed redundant files, updated UI to align across fragments and activities.

--- app/src/main/java/ie/wit/runappv1/main/MainApp.kt
+++ app/src/main/java/ie/wit/runappv1/main/MainApp.kt
@@ -7,31 +7,17 @@ import timber.log.Timber.i
 
 class MainApp : Application() {
 
-//    val races = RaceMemStore()
-//    val races = RaceJSONMemStore(applicationContext)
     val race = RaceModel()
-//    var users = UserMemStore()
 
     lateinit var users: UserJSONStore
 
     lateinit var races: RaceJSONStore
 
     override fun onCreate() {
-//        race.title = "Test 1"
-//        race.raceDistance="10km"
-//        race.raceDate="10/10/2021"
-//        race.description="Test"
-//        race2.title = "Test 2"
-//        race2.raceDistance="5km"
-//        race2.raceDate="11/10/2021"
-//        race2.description="Test2"
         super.onCreate()
         users = UserJSONMemStore(applicationContext, false)
         races = RaceJSONMemStore(applicationContext, false)
-//        races.create(race.copy())
-//        races.create(race2.copy())
         Timber.plant(Timber.DebugTree())
         i("App started")
-        println(races)
     }
 }
--- app/src/main/java/ie/wit/runappv1/models/RaceStore.kt
+++
@@ -1,7 +0,0 @@
-package ie.wit.runappv1.models
-
-interface RaceStore {
-    fun findAll(): List<RaceModel>
-    fun create(race: RaceModel)
-    fun update(race: RaceModel)
-}
